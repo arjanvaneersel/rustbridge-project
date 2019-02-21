9c8c97042b53ff9b33c0cc19bcaf77e90b345b2c
Arjan van Eersel <arjan@vaneersel.eu>
2019-02-21 11:00:42 +0200

    Updated README

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let app = Server::new(|_request, mut response| {
         //let html = String::from("<html><head><title>Rustbridge</title></head><body><h1>Hello Rust!</h1></body></html>").into_bytes();
         let motivation = templates::motivation();
-        Ok(response.header("Content-Type", "text/html; charset=utf-8").body(html)?)
+        Ok(response.header("Content-Type", "text/html; charset=utf-8").body(motivation)?)
     });
 
     let host = "0.0.0.0";
